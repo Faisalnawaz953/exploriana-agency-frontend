256132faf9b3e98901a2fa03f641b7557bf87db4
DESKTOP-03VGQ8N\Luminogics <asimmehmood807@gmail.com>
2021-07-09 16:20:46 +0500

    backend link live added

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,9 +1,8 @@
-export const BASE_URL = "http://localhost:5000/api/";
-
-// export const BASE_URL = "https://movee-app-backend.herokuapp.com/api/";
-export const NETWORK_ERROR = "NETWORK_ERROR";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const UPDATE_USER = "UPDATE_USER";
-export const UPDATE_BRAND = "UPDATE_BRAND";
-export const UPDATE_NOTIFICATIONS_SETTINGS = "UPDATE_NOTIFICATIONS_SETTINGS";
-export const UPDATE_LANDING_PAGE = " UPDATE_LANDING_PAGE";
+//export const BASE_URL = "http://localhost:5000/api/";
+export const BASE_URL = 'https://movee-app-backend.herokuapp.com/api/';
+export const NETWORK_ERROR = 'NETWORK_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const UPDATE_USER = 'UPDATE_USER';
+export const UPDATE_BRAND = 'UPDATE_BRAND';
+export const UPDATE_NOTIFICATIONS_SETTINGS = 'UPDATE_NOTIFICATIONS_SETTINGS';
+export const UPDATE_LANDING_PAGE = ' UPDATE_LANDING_PAGE';
